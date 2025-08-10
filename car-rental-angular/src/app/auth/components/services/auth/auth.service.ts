86e6cf1faaa93c2473204041565583778d597698
avulakishore11 <134989441+avulakishore11@users.noreply.github.com>
2025-08-10 13:58:34 -0400

    Update auth.service.ts

--- car-rental-angular/src/app/auth/components/services/auth/auth.service.ts
+++ car-rental-angular/src/app/auth/components/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 
-const BASE_URL = ['http://localhost:8080']
+const BASE_URL = '/api';     // ['http://localhost:8080']
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +11,10 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   register(signupRequest: any): Observable<any> {
-    return this.http.post(`${BASE_URL}/api/auth/signup`, signupRequest)
+    return this.http.post(`${BASE_URL}/auth/signup`, signupRequest)
   }
 
   login(loginRequest: any): Observable<any> {
-    return this.http.post(`${BASE_URL}/api/auth/login`, loginRequest)
+    return this.http.post(`${BASE_URL}/auth/login`, loginRequest)
   }
 }
